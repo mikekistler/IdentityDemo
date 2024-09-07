495e1d5eac7fda5ac3238d75b4822ea9821abef7
Mike Kistler <mikekistler@microsoft.com>
2024-09-07 10:46:02 -0500

    Working app

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// This is not a JWT token, it's a Bearer token
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthorizationBuilder();
+
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlite("Data Source=app.db"));
+
+builder.Services.AddIdentityCore<MyUser>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapIdentityApi<MyUser>();
+
+app.MapGet("/", (ClaimsPrincipal user) => $"Hello {user.Identity!.Name}")
+    .RequireAuthorization();
 
 app.Run();
+
+class MyUser : IdentityUser;
+class AppDbContext : IdentityDbContext<MyUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
